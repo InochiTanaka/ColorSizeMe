59987d75bb8026444b2b25f1b48d01077f2466e2
InochiTanaka <yunominosencha@gmail.com>
2018-05-09 15:11:28 -0700

    resolve problems by reloading page (login problem : does not accept login on login->logout->login on no reloadingpage. logout problem: popup another login window when login->logout on no reloading page

--- public/JS/Login.js
+++ public/JS/Login.js
@@ -14,21 +14,22 @@ window.onload = function()
 				{
 					document.getElementById("email").style.display="none";
 					document.getElementById("password").style.display="none";
-					document.getElementById("messageText").style.display="inline";
-					document.getElementById("logout").style.display="none";
+					document.getElementById("messageText0").style.display="inline";
+					document.getElementById("messageText1").style.display="inline";
 					document.getElementById("login").style.display="none";
 					document.getElementById("close").style.display="block";
-					messageText.textContent = "Welcome, "+user.email+" ! ";	
+					messageText0.textContent = "Welcome, "+user.email+" ! ";
+					messageText1.textContent = "This window will be closed on later.";					
 				}
 				else
 				{
 					document.getElementById("messageText").style.display="block";
 					document.getElementById("email").style.display="none";
 					document.getElementById("password").style.display="none";
-					document.getElementById("logout").style.display="inline";
 					document.getElementById("login").style.display="none";
 					document.getElementById("close").style.display="block";
-					messageText.textContent = "Please activate your account on Valification Email. Or, please Sign in another account";	
+					messageText0.textContent = "Please activate your account on Valification Email. Or, please Sign in another account";	
+					messageText1.textContent = "";	
 					//alert("Please activate your account on Valification Email.\n Or, please Sign in another account");
 				}		
 			}		
@@ -36,70 +37,32 @@ window.onload = function()
 			{			
 				document.getElementById("email").style.display="block";
 				document.getElementById("password").style.display="block";
-				document.getElementById("messageText").style.display="none";
-				document.getElementById("logout").style.display="none";
+				document.getElementById("messageText0").style.display="none";
+				document.getElementById("messageText1").style.display="none";
 				document.getElementById("login").style.display="block";
 				document.getElementById("close").style.display="block";
-				messageText.textContent = "";							
+				messageText0.textContent = "";
+				messageText1.textContent = "";											
 			}	
 	});
 }
 	
 //Set functions for Log in, Sign in, and Log out.
-function execute(id)
+function login()
 {
 	//Get email and password from UI
 	var email = document.getElementById('email').value;
 	var password = document.getElementById('password').value;
 	var messageText = document.getElementById('messageText');
 	
-	if(id == "newuser")//execute when pushed Sign in button
-	{
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
 		
-		location.href = "/registration.html" ;
+		setTimeout("window.opener.location.reload()",5000);
+		setTimeout("if (/Chrome/i.test(navigator.userAgent)) { window.close(); } else { window.open('about:blank', '_self').close(); }",5100);
 		
-		/*
-		firebase.auth().createUserWithEmailAndPassword(email, password)
-		 .then(user => {
-
-			user.sendEmailVerification().then(function() {
-			  //alert("Sent Verification Email");
-			  	//firebase.auth().signOut()
-				//.catch(function(error) {
-				//	alert('Failed to Logout : ' + error.message);
-				//});				  
-			}).catch(function(error) {
-			  alert(error);
-			});
-			
-		  }, err => {
-			alert(err);
-		  });
-		  */
-	}
-	else if(id == "login")//execute when pushed Log in button
-	{			
-		firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-			}).catch(function(error) {
-			alert('Failed to Login : ' + error.message);
-		});	
-	}
-	else if(id == "logout")//execute when pushed Log out button
-	{
-		//Check login/logout
-		firebase.auth().onAuthStateChanged(function(user) {
-			if(user) {	
-				location.reload();			
-				firebase.auth().signOut().then(user => {
-					location.reload();
-				 }, err => {
-				alert(err);
-			});
-			}
-		});
-	}
-	else if(id == "close")//execute when pushed Log in button
-	{			
-		window.open('about:blank','_self').close();
-	}
+		}).catch(function(error) {
+		alert('Failed to Login : ' + error.message);
+	});	
 }
+
+
